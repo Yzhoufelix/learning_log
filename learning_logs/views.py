aa985e96add758e1f290d40f1bc0dd654e1d8e2a
Yzhoufelix <felix.zhouyf.ca@gmail.com>
2019-04-01 00:16:05 -0400

    add new entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -17,6 +17,7 @@ def topics(request):
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
+    # entries = topic.entry_set.order_by('date_added')
     context = {'topic':topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
@@ -34,3 +35,18 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
