293b817e890ff87f9eed6de4074e165c878d8c0b
Yzhoufelix <felix.zhouyf.ca@gmail.com>
2019-04-01 23:35:17 -0400

    Add user registeration

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name= 'users/login.html'), name = 'login'),
 
     # Logout
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
